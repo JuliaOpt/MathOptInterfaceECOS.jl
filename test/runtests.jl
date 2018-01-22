0415db711a638022181403ee6c6165aee24e8c21
Benoît Legat <benoit.legat@gmail.com>
2018-01-22 17:57:06 +0100

    Use InstanceManager to manage instance

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,28 @@
 using MathOptInterfaceECOS
 using Base.Test
 
+using MathOptInterface
+const MOI = MathOptInterface # TODO remove
+
 using MathOptInterfaceTests
 const MOIT = MathOptInterfaceTests
 
-const solver = () -> ECOSInstance()
+using MathOptInterfaceUtilities
+const MOIU = MathOptInterfaceUtilities
+
+MOIU.@instance ECOSInstanceData () (EqualTo, GreaterThan, LessThan) (Zeros, Nonnegatives, Nonpositives, SecondOrderCone, ExponentialCone) () (SingleVariable,) (ScalarAffineFunction,) (VectorOfVariables,) (VectorAffineFunction,)
+
+function solver()
+    instance = MOIU.InstanceManager(ECOSInstanceData{Float64}(), MOIU.Automatic)
+    MOIU.resetsolver!(instance, ECOSInstance())
+    instance
+end
+
 # SOC2 requires 1e-5
 const config = MOIT.TestConfig(atol=1e-5, rtol=1e-5)
 
 @testset "Continuous linear problems" begin
-    MOIT.contlineartest(solver, config)
+    MOIT.contlineartest(solver, config, ["linear10"])
 end
 
 @testset "Continuous conic problems" begin
