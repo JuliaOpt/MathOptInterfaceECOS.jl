a10fe6367916553ca7ec2f87aedda514bdfd705c
Benoît Legat <benoit.legat@gmail.com>
2018-02-07 16:14:27 +0100

    Updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,19 +12,13 @@ const MOIU = MathOptInterfaceUtilities
 
 MOIU.@instance ECOSInstanceData () (EqualTo, GreaterThan, LessThan) (Zeros, Nonnegatives, Nonpositives, SecondOrderCone, ExponentialCone) () (SingleVariable,) (ScalarAffineFunction,) (VectorOfVariables,) (VectorAffineFunction,)
 
-function solver()
-    instance = MOIU.InstanceManager(ECOSInstanceData{Float64}(), MOIU.Automatic)
-    MOIU.resetsolver!(instance, ECOSInstance())
-    instance
-end
-
 # SOC2 requires 1e-5
 const config = MOIT.TestConfig(atol=1e-5, rtol=1e-5)
 
 @testset "Continuous linear problems" begin
-    MOIT.contlineartest(solver, config, ["linear10"])
+    MOIT.contlineartest(MOIU.InstanceManager(ECOSInstanceData{Float64}(), ECOSInstance()), config, ["linear10"])
 end
 
 @testset "Continuous conic problems" begin
-    MOIT.contconictest(solver, config, ["rsoc", "geomean", "sdp", "rootdet", "logdet"])
+    MOIT.contconictest(MOIU.InstanceManager(ECOSInstanceData{Float64}(), ECOSInstance()), config, ["rsoc", "geomean", "sdp", "rootdet", "logdet"])
 end
